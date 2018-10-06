7d735f8cd2e02b1fd91d5da7db1000796f55a7da
suusan11 <gawow11@gmail.com>
2018-10-06 09:37:02 -0700

    developed sass environment

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const plumber = require("gulp-plumber");
+
+
+gulp.task('default', ['browserSyncTask'], function () {
+    gulp.watch('./scss/**/*.scss',['scss']);
+    gulp.watch('**/*.html', ['html']);
+});
+
+gulp.task('scss', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            outputStyle: 'expanded'
+        })
+            .on('error', sass.logError))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.reload({ stream:true }))
+});
+
+//BrowserSync
+gulp.task("browserSyncTask", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "index.html"
+        }
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src(['**/*.html'])
+        .pipe(browserSync.reload({ stream:true }))
+});
