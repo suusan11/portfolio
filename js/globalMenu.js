30b3dd3fec026b50d046d4d2ace208a48973609d
suusan11 <gawow11@gmail.com>
2018-10-22 15:59:43 -0700

    created click event

---
+++ js/globalMenu.js
@@ -0,0 +1,21 @@
+function navToggle() {
+    var toggleBtn = document.getElementById("nav-toggle");
+    var navShow = document.getElementById("nav-list");
+    var toggleBtnClass = toggleBtn.getAttribute("class");
+
+    if(toggleBtnClass === "nav-toggle-btn close") {
+        toggleBtn.classList.remove("close");
+        navShow.classList.remove("close");
+
+        toggleBtn.classList.add("open");
+        navShow.classList.add("open");
+    }else {
+        toggleBtn.classList.remove("open");
+        navShow.classList.remove("open");
+
+        toggleBtn.classList.add("close");
+        navShow.classList.add("close");
+    }
+}
+
+document.getElementById("nav-toggle").onclick = navToggle;
