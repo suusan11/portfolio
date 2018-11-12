6c0b2f22978a7e85a7b5922d56f066784d6077ad
suusan11 <gawow11@gmail.com>
2018-11-12 15:51:02 -0800

    changed header design

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 //global menu
-function navToggle() {
-    var toggleBtn = document.getElementById("js-nav-toggle");
-    var navShow = document.getElementById("js-circle-nav-list");
-
-    if(toggleBtn.classList.contains("open") === false) {
-        toggleBtn.classList.add("open");
-        navShow.classList.add("open");
-    }else {
-        toggleBtn.classList.remove("open");
-        navShow.classList.remove("open");
-    }
-}
+// function navToggle() {
+//     var toggleBtn = document.getElementById("js-nav-toggle");
+//     var navShow = document.getElementById("js-circle-nav-list");
+//
+//     if(toggleBtn.classList.contains("open") === false) {
+//         toggleBtn.classList.add("open");
+//         navShow.classList.add("open");
+//     }else {
+//         toggleBtn.classList.remove("open").add("close");
+//         // toggleBtn.classList.add("close");
+//         navShow.classList.remove("open");
+//     }
 
 document.getElementById("js-nav-toggle").addEventListener("click", navToggle);
 
