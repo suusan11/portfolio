7ac114fe738c8a0968f8336cdbd077beeb05283b
suusan11 <gawow11@gmail.com>
2018-11-14 17:24:32 -0800

    added new function to animate of text

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+//text effect for name on top
+var fullName = document.getElementsByTagName('h1')[0];
+
+function textEffect(animationName) {
+    let text = fullName.innerHTML;
+    let newText = '';
+    let animation = animationName;
+
+    for(let i = 0; i < text.length; i++) {
+        newText += '<span>' + text.charAt(i) + '</span>';
+    }
+
+    fullName.innerHTML = newText;
+
+    var wrappedChars = document.getElementsByTagName('span');
+    let j = 0;
+
+    function addEffect() {
+        setTimeout(function () {
+            wrappedChars[j].className = animation;
+            j += 1;
+            if(j < wrappedChars.length) {
+                addEffect();
+            }
+        }, 100);
+    }
+    addEffect();
+}
+textEffect('fallDown');
+
+
 //global menu
 // function navToggle() {
 //     var toggleBtn = document.getElementById("js-nav-toggle");
@@ -52,3 +83,5 @@ document.addEventListener('DOMContentLoaded', function(){
 // }
 //
 // document.getElementById('js-container').addEventListener("DOMContentLoaded", addContainer);
+
+
