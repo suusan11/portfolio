0c9fb16c80af6dc8dbc841ab0a111a955361dbd6
suusan11 <gawow11@gmail.com>
2018-11-23 15:16:28 -0800

    removed comment out

--- js/script.js
+++ js/script.js
@@ -84,24 +84,24 @@ document.getElementById("js-nav-toggle").addEventListener("click", navToggle);
 
 
 //add container by browser size
-// document.addEventListener('DOMContentLoaded', function(){
-//     const pcSize = window.matchMedia('screen and (min-width: 1000px)');
-//     const widthMargin = document.getElementById("js-container");
-//
-//     function checkBreakPoint(pcSize) {
-//         if(pcSize.matches) {
-//             widthMargin.classList.add('container');
-//             widthMargin.classList.remove('works__thumbnails');
-//         }else {
-//             widthMargin.classList.add('works__thumbnails');
-//             widthMargin.classList.remove('container');
-//         }
-//     }
-//
-//     pcSize.addListener(checkBreakPoint);
-//
-//     checkBreakPoint(pcSize);
-// })
+document.addEventListener('DOMContentLoaded', function(){
+    const pcSize = window.matchMedia('screen and (min-width: 1000px)');
+    const widthMargin = document.getElementById("js-container");
+
+    function checkBreakPoint(pcSize) {
+        if(pcSize.matches) {
+            widthMargin.classList.add('container');
+            widthMargin.classList.remove('works__thumbnails');
+        }else {
+            widthMargin.classList.add('works__thumbnails');
+            widthMargin.classList.remove('container');
+        }
+    }
+
+    pcSize.addListener(checkBreakPoint);
+
+    checkBreakPoint(pcSize);
+});
 
 // function addContainer() {
 //     const pcSize = window.matchMedia('screen and (min-width: 1000px)');
