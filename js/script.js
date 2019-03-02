d3d24289bf359f918577740d906b7ebbb36cb82e
suusan11 <gawow11@gmail.com>
2019-03-01 23:38:06 -0800

    comment out to extra definition

--- js/script.js
+++ js/script.js
@@ -30,23 +30,23 @@
 
 
 //global menu
-function navToggle() {
-    var toggleBtn = document.getElementById("js-nav-toggle");
-    var navShow = document.getElementById("js-global-nav-list");
-
-    if (toggleBtn.classList.contains("open") === false) {
-        toggleBtn.classList.add("open");
-        navShow.classList.add("open");
-    } else {
-        toggleBtn.classList.remove("open");
-        navShow.classList.remove("open");
-    }
-
-    var tagA = document.getElementById("js-global-nav-list").getElementsByTagName('a');
-    for(let i = 0; i < tagA.length; i++) {
-        tagA[i].addEventListener("click", navToggle);
-    }
-}
+// function navToggle() {
+//     var toggleBtn = document.getElementById("js-nav-toggle");
+//     var navShow = document.getElementById("js-global-nav-list");
+//
+//     if (toggleBtn.classList.contains("open") === false) {
+//         toggleBtn.classList.add("open");
+//         navShow.classList.add("open");
+//     } else {
+//         toggleBtn.classList.remove("open");
+//         navShow.classList.remove("open");
+//     }
+//
+//     var tagA = document.getElementById("js-global-nav-list").getElementsByTagName('a');
+//     for(let i = 0; i < tagA.length; i++) {
+//         tagA[i].addEventListener("click", navToggle);
+//     }
+// }
 
 document.getElementById("js-nav-toggle").addEventListener("click", navToggle);
 
